d779c22e8bda86bbf8bc70ebfdc0112c784c1f5e
izabelsoares <izabelsoaresbds@gmail.com>
2021-05-19 20:26:42 -0300

    exercicio sobre array

---
+++ script.js
@@ -0,0 +1,20 @@
+let itens = ["Mario", "Naruto", "Goku", "Link", "BobEsponja"]
+console.log(itens)
+
+let teste = itens.unshift("Gumball")
+console.log(itens)
+
+let test = itens.pop()
+console.log(itens)
+
+let testi = itens.push("Bolt", "Chaves")
+console.log(itens)
+
+let testo = itens.shift()
+console.log(itens)
+
+let num = [7,5,6,3,8,9,2,1,4]
+console.log(itens)
+
+let nums = num.sort()
+console.log(num)
